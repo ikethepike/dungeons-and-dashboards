a056ef843bd2cf7a82b3ea93b57f94dea000cf43
Daniel Dennerkrans <dennerkrans@gmail.com>
2019-03-21 12:06:44 +0100

    Renamed the firebase env vars

--- src/main.js
+++ src/main.js
@@ -8,16 +8,16 @@ import './registerServiceWorker';
 Vue.config.productionTip = false;
 
 const config = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 
-console.log(process.env.VUE_APP_API_KEY);
+console.log(process.env.VUE_APP_FIREBASE_API_KEY);
 
 new Vue({
   router,
