ab2fe6a3251370816a9d488244f3b9744b57e9c5
Daniel Dennerkrans <dennerkrans@gmail.com>
2019-03-21 12:01:56 +0100

    Added firebase

--- src/main.js
+++ src/main.js
@@ -1,13 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import firebase from 'firebase';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const config = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
+
+console.log(process.env.VUE_APP_API_KEY);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
