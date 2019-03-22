e727d7027a461467301544ae6254476d168d70d9
Daniel Dennerkrans <dennerkrans@gmail.com>
2019-03-22 15:19:29 +0100

    added dashboard view to try dynamic loading from firestore

--- src/router.js
+++ src/router.js
@@ -20,6 +20,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/dashboard/:id',
+      name: 'dashboard',
+      component: () => import(/* webpackChunkName: "about" */ './views/Dashboard.vue')
     }
   ]
 })
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dashboard">
+    <h1 v-if="dashboard">{{ dashboard.name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dashboard: null
+  }),
+  mounted() {
+    this.$db
+      .collection("dashboards")
+      .doc(this.$route.params.id)
+      .onSnapshot(doc => (this.dashboard = doc.data()));
+  }
+};
+</script>
+
