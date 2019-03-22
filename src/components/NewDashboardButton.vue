54ecd3ad6a832500b44f8e235377023fa9f875b3
Daniel Dennerkrans <dennerkrans@gmail.com>
2019-03-22 15:17:21 +0100

    created "newdashboardbutton" component

---
+++ src/components/NewDashboardButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="newDashboard">New dashboard</button>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "newDashboardButton",
+  methods: {
+    newDashboard() {
+      this.$db
+        .collection("dashboards")
+        .add({ name: "New Dashboard" })
+        .then(docRef =>
+          this.$router.push({ name: "dashboard", params: { id: docRef.id } })
+        );
+    }
+  }
+};
+</script>
