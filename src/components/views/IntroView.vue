83db4758765146d7c33664fb9803334ee2d69773
Isaac Kuehnle-Nelson <ikethepike@outlook.com>
2019-03-30 06:51:14 +0100

    Lil d20

--- src/components/views/IntroView.vue
+++ src/components/views/IntroView.vue
@@ -11,6 +11,7 @@ export default {
     // Three objects
     die: null,
     scene: null,
+    light: null,
     camera: null,
     renderer: null
   }),
@@ -22,19 +23,29 @@ export default {
     },
     render() {
       requestAnimationFrame(this.render); // Let's loop it
+      this.die.rotation.y += 0.01;
+      this.die.rotation.x += 0.001;
       this.renderer.render(this.scene, this.camera);
     }
   },
   mounted() {
     // Let's emit an event on completion
     // Let's instantiate a renderer
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
     // Let's create a scene
     this.scene = new THREE.Scene();
 
     // Use orthographic camera to not deal with any FOV distortion
-    this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 1, 1000);
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    );
+
+    this.light = new THREE.SpotLight(0xffffff);
+    this.light.position.z = 5;
 
     // Let's bind the viewport to current dimensions
     this.updateViewport();
@@ -43,18 +54,23 @@ export default {
     this.$refs.intro.appendChild(this.renderer.domElement);
 
     // Let's place the camera
-    this.camera.position.z = 1;
+    this.camera.position.z = 5;
 
     // Let's create our die
     this.die = new THREE.Mesh(
-      new THREE.IcosahedronBufferGeometry(0.5),
-      new THREE.MeshBasicMaterial({ color: 0xd64541 })
+      new THREE.IcosahedronBufferGeometry(1),
+      new THREE.MeshLambertMaterial({ color: 0xd64541 })
     );
+
+    this.die.position.z = -1;
+
+    // Add our lighting
+    this.scene.add(this.light)
 
     // Throw the dice into the scene
     this.scene.add(this.die);
 
-    this.render()
+    this.render();
   }
 };
 </script>
